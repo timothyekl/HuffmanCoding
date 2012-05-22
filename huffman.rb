97d1e2994c446781f0269d8c95831a0ead75630a
Tim Ekl <lithium3141@gmail.com>
2012-05-22 09:42:34 -0400

    Stub actual en/decryption methods
    Signed-off-by: Tim Ekl <lithium3141@gmail.com>

--- huffman.rb
+++ huffman.rb
@@ -4,6 +4,9 @@
 # http://en.nerdaholyc.com/huffman-coding-on-a-string/
 
 require 'optparse'
+require 'pp'
+
+require './coder.rb'
 
 options = {}
 OptionParser.new do |opts|
@@ -19,4 +22,17 @@ if options['mode'].nil?
   Kernel::exit(FALSE)
 end
 
-puts "Mode is #{options['mode']}"
+if ARGV.length == 0
+  puts "You must specify some text to #{options['mode']}"
+  Kernel::exit(FALSE)
+end
+
+case options['mode']
+  when :encode
+    puts HuffmanCoder::encode(ARGV.join(" "))
+  when :decode
+    puts HuffmanCoder::decode(ARGV.join(" "))
+  else
+    puts "Invalid mode"
+    Kernel::exit(FALSE)
+end
